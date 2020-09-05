0a29c4d4679b380621b57d78504c7211a282aacb
VikasJKatariya <vikaskatariya16081998@gmail.com>
2020-09-06 00:08:43 +0530

    --add new array function

---
+++ array_flip.php
@@ -0,0 +1,20 @@
+<?php
+
+//The array_flip() function flips/exchanges all keys with their associated values in an array.
+############################################################################################
+
+$a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
+$result=array_flip($a1);
+
+echo '<pre>';
+print_r($result);
+
+//output : 
+/*Array
+(
+    [red] => a
+    [green] => b
+    [blue] => c
+    [yellow] => d
+)*/
+?>
